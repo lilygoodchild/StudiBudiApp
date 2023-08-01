7c9c2fd20e00ad15505f547ee1677e967dcfeaff
Lily <lsmgoodchild@icloud.com>
2023-08-01 09:35:37 +0100

    Initial Commit

---
+++ StudiBudiApp/StudiBudiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudiBudiAppApp.swift
+//  StudiBudiApp
+//
+//  Created by scholar on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct StudiBudiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
