09776f8e730883bf1efdec3a439756a6eb3be785
Lily <lsmgoodchild@icloud.com>
2023-08-03 14:12:38 +0100

    added calendar

--- CalendarPage.swift
+++ CalendarPage.swift
@@ -9,76 +9,117 @@ import SwiftUI
 
 struct CalendarPage: View {
     @State var selectedDate: Date = Date()
-    
-    @State private var isBlueClicked = false
-    @State private var isGreenClicked = false
-    @State private var isYellowClicked = false
-    @State private var isOrangeClicked = false
-    @State private var isRedClicked = false
- 
-    
+    @State  var backgroundColor = Color.white
     
     var body: some View {
+        
+        VStack(spacing: 40.0) {
+          
+            VStack(spacing: -200.0){
+            Text("Calendar")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+                HStack {
+                    Image("snail")
+                        .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .frame(width: 60.0, height : 450.0)
+                    Text("Sally the Snail: Welcome to your calendar, and mood tracker!")
+                }
+                Divider()
 
-        
-        
-        VStack(alignment: .center, spacing: 0) {
-            Text(selectedDate.formatted(date: .abbreviated, time: .standard))
-                .font(.system(size: 28))
-                .bold()
-                .foregroundColor(Color(red: 255/255, green: 194/255, blue: 180/255))
-                .padding()
-                .animation(.spring(), value: selectedDate)
-            Divider()
-            Spacer()
-            DatePicker("Select Date", selection: $selectedDate)
-                .padding(.horizontal)
-                .datePickerStyle(.graphical)
-                
-            // the above is the calendar itself
+            }
             
-            
-            
-                
-            ZStack{
-                VStack(alignment: .leading, spacing: 10.0){
-                    Text("Mood")
+            VStack(spacing: -200.0){
+                VStack{
+                    Text(selectedDate.formatted(date: .abbreviated, time: .standard))
+                        .font(.system(size: 28))
+                        .bold()
+                        .foregroundColor(Color(red: 255/255, green: 194/255, blue: 180/255))
                     
-                    HStack(spacing: 27.0){
-                        
-                        Button("Red") {
-                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                            
-                            
-                        }.foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                        
-                        Button("Orange") {
-                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                        }.foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                        Button("Yellow") {
-                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                        }.foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                        Button("Green") {
-                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                        }
-                        .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                        Button("Blue") {
-                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                        }.foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                        
-                    }
+                        .animation(.spring(), value: selectedDate)
                     
+                    
+                    
+                    DatePicker("Select Date", selection: $selectedDate)
+                        .padding(.horizontal)
+                        .datePickerStyle(.graphical)
+                    
+                    Text("Today's mood :")
+                    Spacer()
                     
                 }
-            }.padding(.all, 21.0).background(Color(red: 255/255, green: 194/255, blue: 180/255)).cornerRadius(20)
+                
+                
+                HStack{
+                    Image("happiest")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit).clipShape(Circle())
+                        .frame(width: 51.0, height : 450.0)
+                        .onTapGesture {
+                            backgroundColor = Color(red: 59/255, green: 205/255, blue: 81/255)}
+                    
+                    
+                    
+                    Image("happy")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit).clipShape(Circle())
+                        .frame(width: 51.0, height : 450.0)
+                        .onTapGesture {
+                            backgroundColor = Color(red: 178/255, green: 200/255, blue: 67/255) }
+                    
+                    Image("mid")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit).clipShape(Circle())
+                        .frame(width: 51.0, height : 450.0)
+                        .onTapGesture {
+                            backgroundColor = Color(red: 245/255, green: 206/255, blue: 56/255)}
+                    
+                    Image("sad")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit).clipShape(Circle())
+                        .frame(width: 51.0, height : 450.0)
+                        .onTapGesture {
+                            backgroundColor = Color(red: 254/255, green: 107/255, blue: 38/255)}
+                    
+                    Image("angry")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit).clipShape(Circle())
+                        .frame(width: 51.0, height : 450.0)
+                        .onTapGesture {
+                            backgroundColor = Color(red: 252/255, green: 60/255, blue: 48/255)}
+                    
+                }
+                
+               
+               
+                Rectangle()
+                    .padding(.all, 33.0)
+                    .foregroundColor(backgroundColor)
+                
+            }.padding()
+            
+            
         }
-        .padding(.vertical, 200)
+        .padding(.top, 1.0)
+        
+        
+        
+        
+        
+        
+            
+         
         
     }
 }
 
-struct CalendarPage_Previews: PreviewProvider {
-    static var previews: some View {
-        CalendarPage()
-    }
-}
+        
+        struct CalendarPage_Previews: PreviewProvider {
+            static var previews: some View {
+                CalendarPage()
+            }
+        }
+
