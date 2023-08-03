4e978cfc302d5d649528420b36327951573aee1b
Emma <emmalynes@aol.com>
2023-08-03 15:31:57 +0100

    final minus news

--- StudiBudiApp/ContentView.swift
+++ StudiBudiApp/ContentView.swift
@@ -9,19 +9,149 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            ZStack {
+                Color(red: 255/255, green: 255/255, blue: 255/255)
+                .ignoresSafeArea()
+                
+               
+                VStack(alignment: .center ,spacing: 5.0) {
+                    Text("Home")
+                        .font(.title)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.black)
+                    Spacer()
+                }
+                
+                
+                
+                VStack {
+                
+                    
+                    Image("appicon")
+                    // .resizable(resizingMode: .stretch)
+                    //.aspectRatio(contentMode: .fit)
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .frame(width: 250.0, height: 250.0)
+                    
+                    
+                    
+                    Text("Hi, Welcome to Studi Buddi")
+                        .font(.title3)
+                        .fontWeight(.regular)
+                        .foregroundColor(Color.black)
+                
+                
+                    
+                    
+                    
+                }
+                VStack(spacing: 650) {
+                    Text("")
+                    Divider()
+                        .frame(height: 3)
+                        .overlay(.black)
+                }
+                
+                
+                
+                .toolbar {
+                    
+                    ToolbarItemGroup(placement: .status) {
+                       
+                        HStack {
+                            
+                            NavigationLink(destination: CalendarPage()) {
+                                Image(systemName: "calendar")
+                                    .resizable(resizingMode: .stretch)
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(.black)
+                                Spacer()
+                                
+                                
+                                
+                            }
+                            
+                            
+                            NavigationLink(destination: NewsPage()) {
+                                Image(systemName: "newspaper")
+                                    .resizable(resizingMode: .stretch)
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(.black)
+                                Spacer()
+                                
+                            }
+                            
+                            
+                            NavigationLink(destination: HomePage()) {
+                                Image(systemName: "house")
+                                    .resizable(resizingMode: .stretch)
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(.black)
+                                Spacer()
+
+                                
+                                
+                            }
+                            
+                            
+                            
+      //hi
+                            
+                            NavigationLink(destination: ResourcesPage()) {
+                                Image("resourcesicon")
+                                    .resizable(resizingMode: .stretch)
+                                    .aspectRatio(contentMode: .fit)
+                                Spacer()
+                                
+                                
+
+                                
+                            }
+                            
+                            NavigationLink(destination: QuotesPage()) {
+                            Image(systemName: "quote.bubble")
+                                    
+                                    .resizable(resizingMode: .stretch)
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(.black)
+                                
+                            }
+                            
+                            
+        
+                            
+                        }
+                        
+                    }
+                    
+                    
+                }
+                
+                
+            }
+            //
         }
-        .padding()
+        //              .toolbar {
+        //                  Image(systemName: "newspaper")
+        //
+        //                   // .toolbar {
+        //                        //Image(systemName: "")
+        //                 //}
+        //
+        //
+        //
     }
-}
-//hi
-//lily
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
+    
+    
 }
