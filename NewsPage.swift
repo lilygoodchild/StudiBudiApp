8a7b30ddd5e78c1c2f6ed852f762c881639fa2bc
Lily <lsmgoodchild@icloud.com>
2023-08-03 15:36:14 +0100

    news added

--- NewsPage.swift
+++ NewsPage.swift
@@ -10,109 +10,142 @@ import SwiftUI
 
 
 struct NewsPage: View {
-
+    
     var body: some View {
-
-        VStack(spacing: 20) {
-
-            Text("Latest News")
-
-                .padding()
-
-                .font(.largeTitle)
-
-                .foregroundColor(.blue)
-
-                .padding(.top, 40.0)
-
+        ZStack {
             
+          
 
-            LinkButton(title: "News Article 1", url: "https://caitlinhouston.medium.com/10-ways-reset-your-mind-when-youre-feeling-overwhelmed-dc5ba6ebf3c7")
-
-            LinkButton(title: "News Article 2", url: "https://caitlinhouston.medium.com/10-ways-reset-your-mind-when-youre-feeling-overwhelmed-dc5ba6ebf3c7")
-
-            LinkButton(title: "News Article 3", url: "https://medium.com/@elif.sht6/7-important-ways-to-maintain-your-mental-health-a6a5427142f7")
-
-            
-
-            Spacer()
-
-        }
-
-        .padding()
-
-    }
-
-    struct LinkButton: View {
-
-        let title: String
-
-        let url: String
-
-        
-
-        var body: some View {
-
-            Button(action: {
-
-                openNewsArticle(url: url)
-
-            }) {
-
+            VStack(alignment: .center, spacing: 5) {
+                
+                    Text("News")
+                    
+                    
+                    
+                    .font(.largeTitle)
+                        .foregroundColor(.black)
+                        .bold()
+                        
+                
+                    //final iteration
+                
+                                   
+                Spacer()
+                
                 HStack {
-
-                    Image(systemName: "newspaper")
-
-                        .foregroundColor(.blue)
-
+                        Image("tally")
+                        .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                        .scaledToFit()
+                        .frame(width: 80.0, height : 43.0)
+                        Text("Tally the turtle: access useful articles for your mental health!")
+                                           
+                                   }
+                        Divider()
+                          .padding(.bottom, 2.0).padding(.top,1.0)
+                
+               
+                    
+                    LinkButton(title: "Confidence and Self Esteem", url: "https://www.mind.org.uk/for-young-people/feelings-and-experiences/confidence-and-self-esteem/?gclid=CjwKCAjw_aemBhBLEiwAT98FMmJ7aFprl89IlVtq-YpNcKJvCa964RXl8gUpMHH_-GPovwli-NZhHhoCkCMQAvD_BwE",imageName: "confidence")
+                    
+                    LinkButton(title: "Introduction into Mental Health ", url: "https://www.mind.org.uk/for-young-people/introduction-to-mental-health/",imageName: "introductionintomentalhealth")
+                    
+                    LinkButton(title: "Looking after your Mental Health as a Student", url: "https://medium.com/@elif.sht6/7-important-ways-to-maintain-your-mental-health-a6a5427142f7",imageName:"student")
+                    
+                
+                
+                Spacer()
+                
+            }
+            .padding(.top, -3.0)
+            
+        }
+        
+        .padding()
+        
+    }
+    
+    struct LinkButton: View {
+        
+        let title: String
+        
+        let url: String
+        
+        let imageName: String
+        
+        
+        
+        
+        var body: some View {
+            
+            Button(action: {
+                
+                openNewsArticle(url: url)
+                
+            }) {
+                VStack(spacing: 13.0) {
+                    Image(imageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .padding(.top, 45.0)
+                        .cornerRadius(10)
+                        .frame(width: 190.0, height: 80.0)
+                        .foregroundColor(.black)
+                       
+                    Spacer()
+                    
+                    Text(title)
+                
+                        .foregroundColor(.black)
+                        .padding(.vertical, 10.0)
+                        .frame(width: 190.0, height: 90.0)
+                }
+                .padding([.top, .leading, .trailing])
+                .background(Color(red: 211/255.0, green: 181/255.0, blue: 255/255.0).opacity(0.2))
+                .cornerRadius(10)
+                
+                HStack {
                     
 
-                    Text(title)
-
-                        .foregroundColor(.blue)
-
-                        .font(.headline)
-
+                    
                 }
-
-                .padding()
-
-                .background(Color.gray.opacity(0.2))
-
+            
+                
                 .cornerRadius(10)
-
+                
             }
-
+            
         }
-
+        
         func openNewsArticle(url: String) {
-
+            
             if let articleURL = URL(string: url) {
-
+                
                 UIApplication.shared.open(articleURL)
-
+                
             }
-
+            
         }
-
-    }
-
-    
-
-    //hi
-
-    
-
-    
-
-    struct NewsPage_Previews: PreviewProvider {
-
-        static var previews: some View {
-
-            NewsPage()
-
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        struct NewsPage_Previews: PreviewProvider {
+            
+            static var previews: some View {
+                
+                NewsPage()
+                
+            }
+            
         }
-
+        
+        
     }
-
 }
