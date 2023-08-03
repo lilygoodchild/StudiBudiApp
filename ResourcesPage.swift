9b38c14568e1eab484db886ba18342b1d61aeffc
Lily <lsmgoodchild@icloud.com>
2023-08-03 14:47:59 +0100

    resources

--- ResourcesPage.swift
+++ ResourcesPage.swift
@@ -24,7 +24,7 @@ struct LinkButton: View {
 }
 
 
-struct ResourcesPageView: View {
+struct ResourcesPage: View {
     var body: some View {
         
         
@@ -44,136 +44,119 @@ struct ResourcesPageView: View {
                 }.padding(.bottom, 15.0)
                 
                 
-                VStack(alignment: .center, spacing: 15.0){
-                    
+ VStack(alignment: .center, spacing: 15.0){
+
                     VStack(alignment: .center, spacing: 6.0){
                         
                         Text("English")
-                        
+
                         HStack(spacing: 15.0){
-                            
+
+                            LinkButton(imageName: "massoit", urlString: "https://www.google.com")
+
+
+                            LinkButton(imageName: "sparknotes", urlString: "https://www.google.com")
+
+
+                            LinkButton(imageName: "mrbruff", urlString: "https://www.google.com")
+
+
+                            LinkButton(imageName: "saveexams", urlString: "https://www.google.com")
+
+
                             LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
+
                         }.padding(.horizontal, 15.0)
                     }.padding(.top,10.0).padding(.bottom, 10.0).background(Color(red: 167/255, green: 212/255, blue: 155/255)).cornerRadius(15).shadow(radius: 3)
-                    
-                    
+
+//
                     VStack(alignment: .center, spacing: 6.0){
-                        
+
                         Text("Maths")
-                        
+
                         HStack(spacing: 15.0){
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
+                            LinkButton(imageName: "mathswatch", urlString: "https://vle.mathswatch.co.uk/vle/")
+
+
+                            LinkButton(imageName: "mathgenie", urlString: "https://www.mathsgenie.co.uk/")
+
+                            LinkButton(imageName: "TLMaths", urlString:
+                                        "https://www.youtube.com/c/TLMaths")
+
+
+                            LinkButton(imageName: "mathstutor", urlString: "https://www.youtube.com/@TheGCSEMathsTutor")
+
+
+                            LinkButton(imageName: "pmt", urlString: "https://www.physicsandmathstutor.com/maths-revision/")
+
                         }.padding(.horizontal, 15.0)
                     }.padding(.top,10.0).padding(.bottom, 10.0).background(Color(red: 167/255, green: 212/255, blue: 155/255)).cornerRadius(15).shadow(radius: 3)
-                    
-                    VStack(alignment: .center, spacing: 6.0){
-                        
-                        Text("History")
-                        
-                        HStack(spacing: 15.0){
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                        }.padding(.horizontal, 15.0)
-                    }.padding(.top,10.0).padding(.bottom, 10.0).background(Color(red: 167/255, green: 212/255, blue: 155/255)).cornerRadius(15).shadow(radius: 3)
+
                     
                     
                     VStack(alignment: .center, spacing: 6.0){
-                        
+
                         Text("Geography")
-                        
+
                         HStack(spacing: 15.0){
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
+                            LinkButton(imageName: "pmt", urlString: " https://www.physicsandmathstutor.com/geography-revision/")
+
+                            LinkButton(imageName: "link", urlString: "https://www.bbc.co.uk/bitesize/subjects/z2f3cdm")
+
+                            LinkButton(imageName: "rgs", urlString: " https://www.rgs.org/")
+
+
+                            LinkButton(imageName: "seneca", urlString: "https://senecalearning.com/en-GB/")
+
+
+                            LinkButton(imageName: "tutor2u", urlString: " https://www.tutor2u.net/geography")
+
                         }.padding(.horizontal, 15.0)
                     }.padding(.top,10.0).padding(.bottom, 10.0).background(Color(red: 167/255, green: 212/255, blue: 155/255)).cornerRadius(15).shadow(radius: 3)
                     
                     
                     VStack(alignment: .center, spacing: 6.0){
-                        
+
                         Text("Science")
-                        
+
                         HStack(spacing: 15.0){
                             LinkButton(imageName: "activelearn", urlString: "https://www.pearsonactivelearn.com")
-                            
+
                             LinkButton(imageName: "crashcourse", urlString: "https://www.youtube.com/channel/UCX6b17PVsYBQ0ip5gyeme-Q")
-                            
+
                             LinkButton(imageName: "uplearn", urlString: "https://uplearn.co.uk/")
-                            
-                            
+
+
                             LinkButton(imageName: "pmt", urlString: "https://www.physicsandmathstutor.com")
-                            
-                            
+
+
                             LinkButton(imageName: "freesciencelessons", urlString: "https://www.freesciencelessons.co.uk")
-                            
+
                         }.padding(.horizontal, 15.0)
                     }.padding(.top,10.0).padding(.bottom, 10.0).background(Color(red: 167/255, green: 212/255, blue: 155/255)).cornerRadius(15).shadow(radius: 3)
                     
                     VStack(alignment: .center, spacing: 6.0){
-                        
+
                         Text("French")
-                        
+
                         HStack(spacing: 15.0){
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
-                            
-                            LinkButton(imageName: "link", urlString: "https://www.google.com")
-                            
+                            LinkButton(imageName: "linguascope", urlString: "https://www.linguascope.com/")
+
+                            LinkButton(imageName: "seneca", urlString: "https://senecalearning.com/en-GB/")
+
+                            LinkButton(imageName: "quizlet", urlString: "https://quizlet.com/en-gb")
+
+                            LinkButton(imageName: "saveexams", urlString: "https://www.savemyexams.com/gcse/geography/")
+
+                            LinkButton(imageName: "babbel", urlString: "https://uk.babbel.com/pages/en-gb/evergreen-multilanguage?bsc=gg_nb_srh_eng_all&btp=default&gclid=Cj0KCQjwoK2mBhDzARIsADGbjer8cTzAAxov9I2B7L3a_7kWhv0XskzfV-gCHiT1BaVz-uPTkZVFzd8aAiQKEALw_wcB&utm_content=17293993097_136539680813_kwd-28954316_632781806553&utm_medium=cpc&utm_source=google")
+
                         }.padding(.horizontal, 15.0)
                     }.padding(.top,10.0).padding(.bottom, 10.0).background(Color(red: 167/255, green: 212/255, blue: 155/255)).cornerRadius(15).shadow(radius: 3)
                     
                 }
                 .padding(.all, 1.0)
                 
-    
+                
             }
             .padding(.bottom, 2.0)
             
@@ -181,14 +164,14 @@ struct ResourcesPageView: View {
             
         }
     }
+}
     
+
     
-    
-    struct ResourcesPageView_Previews: PreviewProvider {
+    struct ResourcesPage_Previews: PreviewProvider {
         static var previews: some View {
-            ResourcesPageView()
+            ResourcesPage()
         }
     }
     
-}
 
