897769602bf8fff040d9bd5f172b872d903c46cd
Lily <lsmgoodchild@icloud.com>
2023-08-03 14:14:22 +0100

    added home page

--- HomePage.swift
+++ HomePage.swift
@@ -1,20 +1,20 @@
 //
-//  JournalPage.swift
+//  HomePage.swift
 //  StudiBudiApp
 //
-//  Created by scholar on 02/08/2023.
+//  Created by scholar on 03/08/2023.
 //
 
 import SwiftUI
 
-struct JournalPage: View {
+struct HomePage: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct JournalPage_Previews: PreviewProvider {
+struct HomePage_Previews: PreviewProvider {
     static var previews: some View {
-        JournalPage()
+        HomePage()
     }
 }
